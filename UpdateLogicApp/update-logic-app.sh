918b5096e8f73b3fde6bb51d3c8f06b05f10ae1e
timmyreilly <timreilly@live.com>
2019-02-15 16:10:06 -0800

    update network id

--- UpdateLogicApp/update-logic-app.sh
+++ UpdateLogicApp/update-logic-app.sh
@@ -12,7 +12,7 @@ for filename in *.json; do
             
         action="$(jq -r .actions[$i]."action" $filename)"
         contract="$(jq -r .actions[$i]."contract" $filename)"
-        addr="$(jq '.networks."242".address' ../build/contracts/$contract.json)"
+        addr="$(jq '.networks."1337".address' ../build/contracts/$contract.json)"
 
         echo ${resource_group}, ${logic_app}, ${action}, ${addr}
 
@@ -28,7 +28,7 @@ for filename in *.json; do
             
         trigger="$(jq -r .triggers[$i]."trigger" $filename)"
         contract="$(jq -r .triggers[$i]."contract" $filename)"
-        addr="$(jq '.networks."242".address' ../build/contracts/$contract.json)"
+        addr="$(jq '.networks."1337".address' ../build/contracts/$contract.json)"
 
         echo ${resource_group}, ${logic_app}, ${trigger}, ${addr}
 
